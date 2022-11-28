e2a60b1e2eccc59132f9921a18d7cc48b4ec989d
Makiyu-py <73825066+Makiyu-py@users.noreply.github.com>
2022-11-29 07:25:55 +0800

    move fs-related functions on a header file
    will do the same with commands soon

--- mpdfm.cpp
+++ mpdfm.cpp
@@ -1,32 +1,8 @@
-#include <fstream>
 #include <iostream>
-#include <ios>
-#include <cstdio>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <pwd.h>
+#include "mpdfm_utils/fs.h"
 #include "argh.h"
 
 using namespace std;
-
-char* get_home_dir()
-{
-  char *homedir;
-
-  if ((homedir = getenv("HOME")) == NULL)
-  {
-    homedir = getpwuid(getuid())->pw_dir;
-  }
-
-  return homedir;
-}
-
-void create_soft_link(const char *dir_path, const char *link_path)
-{
-  symlink(dir_path, link_path);
-  cout << "Added a symlink to " << link_path << " from " << dir_path << endl;
-}
 
 int main(int, char* argv[])
 {
@@ -34,22 +10,17 @@ int main(int, char* argv[])
 
   string command = cmdl[1];
 
-  char cwd_buffer[256]; // current working dir
-  getcwd(cwd_buffer, sizeof(cwd_buffer));
-  const char *home_dir = get_home_dir();
+  char *cwd_buffer = fs_util::path::get_cwd();
+  const char *home_dir = fs_util::path::get_home();
 
   if (command == "dot")
   {
     string target = cmdl[2];
     string new_dir = string(cwd_buffer) + "/" + ( cmdl[3].empty() ? target.substr(target.find_last_of("/\\") + 1) : cmdl[3] );
 
-    const char *target_cstr = target.data();
-    const char *new_dir_cstr = new_dir.data();
-
     cout << target << " -> " << new_dir << endl;
 
-    // move the dotfile to the repo
-    if (rename(target_cstr, new_dir_cstr) != 0)
+    if (fs_util::path::move(target, new_dir) != 0)
     {
       cerr << "Error moving file!" << endl;
       return 1;
@@ -59,15 +30,30 @@ int main(int, char* argv[])
       cout << "Moved " << target << " to " << new_dir << endl;
     }
 
-   // add symlink to the target
-   create_soft_link(new_dir_cstr, target_cstr);
+   if (fs_util::soft_link::create(new_dir, target) != 0)
+    {
+      cerr << "Error adding a symlink to " << target << " from " << new_dir << endl;
+      return 1;
+    }
+    else
+    {
+      cout << "Added a symlink to " << target << " from " << new_dir << endl;
+    }
   }
   else if (command == "link")
   {
     string fn = string(cwd_buffer) + "/" + cmdl[2];
     string to_symlink = cmdl[3].empty() ? string(home_dir) + "/" + cmdl[2] : cmdl[3];
 
-    create_soft_link(fn.data(), to_symlink.data());
+    if (fs_util::soft_link::create(fn, to_symlink) != 0)
+    {
+      cerr << "Error adding a symlink to " << to_symlink << " from " << fn << endl;
+      return 1;
+    }
+    else
+    {
+      cout << "Added a symlink to " << to_symlink << " from " << fn << endl;
+    }
   }
   else
   {
---
+++ mpdfm_utils/fs.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <pwd.h>
+
+namespace fs_util
+{
+    class path
+    {
+    public:
+        path();
+        static char* get_cwd()
+        {
+            static char cwd_buffer[256];
+            getcwd(cwd_buffer, sizeof(cwd_buffer));
+            
+            return cwd_buffer;
+        }
+        static char* get_home()
+        {
+            char *homedir;
+
+            if ((homedir = getenv("HOME")) == NULL)
+                homedir = getpwuid(getuid())->pw_dir;
+
+            return homedir;
+        }
+        static int move(std::string target, std::string new_dir)
+        {
+            return rename(target.data(), new_dir.data());
+        }
+    };
+
+    class soft_link
+    {
+    public:
+        static int create(std::string dir_path, std::string link_path)
+        {
+            return symlink(dir_path.data(), link_path.data());
+        }
+        static int remove(std::string path)
+        {
+            return unlink(path.data());
+        }
+    };
+    
+
+}
